956f00016351074cb154ee0d2bf740a5fb823078
Sarah <sarahlchavez@gmail.com>
2021-09-29 09:59:20 -0700

    seeded database with preliminary data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 oklahoma = State.create(name: "Oklahoma")
 new_mexico = State.create(name: "New Mexico")
 
-Clinic.create(name: "Trust Women", adress: "1240 SW 44th Street", city: "Oklahoma City", :zip_code "73109", :phone_number "405-429-7940", state: "Oklahoma")
-Clinic.create(name: "UNM Center for Reproductive Health", adress: "2301 Yale Blvd SE suite e", city: "Albuquerque", :zip_code "87106", :phone_number "505-925-4455", state: "New Mexico")
+Clinic.create(name: "Trust Women", address: "1240 SW 44th Street", city: "Oklahoma City", zip_code: "73109", phone_number: "405-429-7940", state: oklahoma)
+Clinic.create(name: "UNM Center for Reproductive Health", address: "2301 Yale Blvd SE suite e", city: "Albuquerque", zip_code: "87106", phone_number: "505-925-4455", state: new_mexico)
 
 
 
