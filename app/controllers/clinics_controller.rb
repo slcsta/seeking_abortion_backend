098b80c8cedf1890facb7a097917c6d52f023190
Sarah <sarahlchavez@gmail.com>
2021-10-03 15:09:24 -0700

    added create method to clinics controller

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -3,4 +3,19 @@ class ClinicsController < ApplicationController
         clinics = Clinic.all
         render json: ClinicSerializer.new(clinics)
     end
+
+    def create
+        clinic = Clinic.new(clinic_params)
+        if clinic.save
+            render json: ClinicSerializer.new(clinic)
+        else
+            render json: {error: "error: failed to save"}
+        end
+    end
+
+    private
+
+    def clinic_params
+        params.require(:clinic).permit(:name, :address, :city, :zip_code, :phone_number)
+    end
 end
