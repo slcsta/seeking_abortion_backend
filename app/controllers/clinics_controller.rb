c6e474a6ade0e49e0028d95b9789f915e63e3e05
Sarah <sarahlchavez@gmail.com>
2021-10-04 10:45:49 -0700

    added destroy method in clinics controller

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -4,13 +4,24 @@ class ClinicsController < ApplicationController
         render json: ClinicSerializer.new(clinics)
     end
 
+    def show
+        
+    end
+
     def create
         clinic = Clinic.new(clinic_params)
+        clinic.state = State.last # setting this equal to the last state for now - will have to go back and deal with this later
         if clinic.save
             render json: ClinicSerializer.new(clinic)
         else
-            render json: {error: "error: failed to save"}
+            render json: {error: "Failed to save"}
         end
+    end
+
+    def destroy
+        clinic = Clinic.find_by_id(params[:id])
+        clinic.destroy
+        render json: {message: "#{clinic.name} successfully deleted"}
     end
 
     private
